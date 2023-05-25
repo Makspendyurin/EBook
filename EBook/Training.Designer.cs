25312abe682df6727a7428db51754156f661508f
Professional <Professional@LAPTOP-ACER>
2023-05-25 15:52:14 +0700

    Добавьте файлы проекта.

---
+++ EBook/Training.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace EBook
+{
+    partial class Training
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(74, 101);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(216, 31);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "О программе";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(74, 34);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(216, 31);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Справочная информация";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Training
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(353, 160);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Training";
+            this.Text = "Обучение";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Training_FormClosed);
+            this.Load += new System.EventHandler(this.Training_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ EBook/Training.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace EBook
+{
+    public partial class Training : Form
+    {
+
+        public Training()
+        {
+            InitializeComponent();
+            this.CenterToScreen();
+           
+        }
+
+        private void Training_Load(object sender, EventArgs e)
+        {
+            
+            MaximizeBox = false;
+        }
+
+        private void Training_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            this.Dispose();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+
+
+            MessageBox.Show("Программа созданна студентом группы И-42, Андреевым Андреем Игоревичем", "О программе");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Process.Start(@"Help.docx");
+        }
+    }
+}
